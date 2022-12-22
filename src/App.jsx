28ad66d1028be228866a3cf2c30a89544234b88d
Miguel Ángel García <92162464+mikaeltevet@users.noreply.github.com>
2022-12-22 12:08:27 -0600

    just stylin'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import WeatherData from '/src/components/WeatherData'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
